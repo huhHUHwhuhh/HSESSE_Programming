9eeaaba81d2dd869c32133c05db2fe026265db3a
HUH HUH WHUH <dmitriychernyshov312@gmail.com>
2023-09-19 11:36:20 +0300

    HW3

---
+++ Seminars/Seminar03/Self/Task01/Program.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Program {
+    static void Main(string[] args) {
+        double l,n;
+        Console.Write("Введите количество сторон: ");
+        do
+        {
+            double.TryParse(Console.ReadLine(), out n);
+            if (n<3 || n > 11)
+            {
+                Console.Write("Количество сторон должно быть больше 3, но меньше 11, введите заново: ");
+            } else 
+            {
+                break;
+            } while(true)    
+        }
+        Console.Write("Введите длину стороны: ");
+        do
+        {
+            double.TryParse(Console.ReadLine(), out l);
+            if (l<=0)
+            {
+                Console.Write("Длина стороны должна быть больше 0, введите заново: ");
+            } else 
+            {
+                break;
+            } while(true)    
+        }
+        double S = (n*l*l)/(4*Math.Tan((Math.PI)/ n));
+        Console.WriteLine("Площадь:" + S)
+    }
+}
