01ebce4c4654fea4836fdc2967622d021dc9f514
huhHUHwhuhh <pivoman2012@gmail.com>
2023-09-26 13:59:07 +0300

    HW5 Task01

---
+++ Seminars/Seminar05/Self1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        for(int x=0;x<=50;x++)    
+        {
+            if(x%6==0)
+            {
+                Console.WriteLine(x+", Число делится на 2 и на 3");
+            }else if(x==20)
+            {
+
+            }else
+            {
+                Console.WriteLine(x+", Число не соответсвует условиям");
+            }
+        }
+    
+    }
+}   
