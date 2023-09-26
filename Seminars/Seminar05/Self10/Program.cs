0c325e4ccbba25a9730a7545b87e46fa8409e70f
huhHUHwhuhh <pivoman2012@gmail.com>
2023-09-26 12:56:23 +0300

    hw5

---
+++ Seminars/Seminar05/Self10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    static double G(double x)
+    {
+        if(x<=0.5)
+        {
+            return(Math.Sin(Math.PI/2));
+        }
+        else
+        {
+            return(Math.Sin(((x-1)*Math.PI)/2));
+        }
+    }
+    static void Main(string[] args)
+    {
+        double x;
+        Console.Write("Введите число: ");
+        double.TryParse(Console.ReadLine(), out x);
+        Console.WriteLine("G(X) = "+ G(x));
+    }
+}   
