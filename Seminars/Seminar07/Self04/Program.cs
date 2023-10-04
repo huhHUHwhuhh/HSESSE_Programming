40f40485f3081ea5f90098c58fc54f347180a5b3
huhHUHwhuhh <pivoman2012@gmail.com>
2023-10-04 20:24:58 +0300

    animation

--- Seminars/Seminar07/Self04/Program.cs
+++ Seminars/Seminar07/Self04/Program.cs
@@ -1,40 +1,126 @@
 ﻿using System;
 public class Program
 {
-    static void Python(int n)
+    
+    
+    static void PythonAnimation(int n)
     {
         (int x, int y) = Console.GetCursorPosition();
-        int PosY = y, PosX = x;
-        for(int k = 0; k<n;k++)//рисуем несколько изломов друг за другом
+        int size = n*4;
+        int[,] indexes = new int[n, size];
+        for (int i = 0; i < n; i++)
         {
-            PosX = x+4*k;
-            for (int i = 0; i < n; i++) //рисует один излом
+            for (int j = size-1; j >= 0; j--)
             {
-                if(i == 0)
+                if(j%2==0)
                 {
-                    Console.SetCursorPosition(PosX,PosY);
-                    Console.Write("***");
+                    indexes[i,j]=1;
                 }
-                else 
+                else if(j%2==1 && (j+1)%4!=0 && i == 0)
                 {
-                    Console.SetCursorPosition(PosX,PosY);
-                    Console.Write("* *");
+                    indexes[0,j]=1;
+                } 
+                else if((j+1)%4==0 && i == n-1)
+                {
+                    indexes[n-1,j]=1;
                 }
-                if(i == n-1)
+            };
+        };
+        indexes[n-1, size-1] = 0;
+
+        // for (int i = 0; i < size; i++)
+        // {
+        //     for (int j = 0; j < size; j++)
+        //     {
+        //         Console.Write(indexes[i,j]);
+        //     };
+        //     Console.WriteLine();
+        // };
+
+        string State = "up";
+        Console.WriteLine();
+        (int PosX, int PosY) = Console.GetCursorPosition();
+
+        for (int j = 0; j < size-1; j++)
+        {
+            if((State == "up") || (State == "up2"))
+            {
+                for (int i = n-1; i >= 0; i--)
                 {
-                    if(k!=n-1)
+                    if(indexes[i,j]==1)
                     {
-                        Console.SetCursorPosition(PosX+3,PosY);
-                        Console.WriteLine("*");
+                        Console.SetCursorPosition(PosX+j,PosY+i);
+                        
+                        Console.Write("*");
+                        Thread.Sleep(200);
                     }
+                    else
+                    {
+                        Console.SetCursorPosition(PosX+j,PosY+i);
+                        Console.Write(" ");
+                    }
+                
                 }
-                PosY++;
+                //Console.Write("**");
+                if(State == "up")
+                {
+                    State = "up2";
+                }
+                else if(State == "up2")
+                {
+                    State = "down";
+                }else if(State == "down")
+                {
+                    State = "down2";
+                }else if(State == "down2")
+                {
+                    State = "up";
+                }
             }
-            PosY = PosY - n;
-        }
+            else if((State == "down") || (State == "down2"))
+            {
+                for (int i = 0; i <= n - 1 ; i++)
+                {
+                    if(indexes[i,j]==1)
+                    {
+                        
+                        Console.SetCursorPosition(PosX+j,PosY+i);
+                        
+                        Console.Write("*");
+                        Thread.Sleep(200);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(PosX+j,PosY+i);
+                        Console.Write(" ");
+                    }
+                
+                }
+                if(State == "up")
+                {
+                    State = "up2";
+                }
+                else if(State == "up2")
+                {
+                    State = "down";
+                }else if(State == "down")
+                {
+                    State = "down2";
+                }else if(State == "down2")
+                {
+                    State = "up";
+                }
+            }
+            
+        };
+        Console.SetCursorPosition(PosX+size,PosY+n);
+        Console.WriteLine();
+
     }
+
     static void Main(string[] args)
     {
-        Python(5);
+        Console.Clear();
+        PythonAnimation(5);
     }
 }  
